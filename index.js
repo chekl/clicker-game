28d92844253c0ba4ba4fa115f953e620d16c79fa
Vladyslava Chaika <vladyslava.chaika@nure.ua>
2023-04-12 14:55:51 +0300

    Fixing bags and edit bg images

--- index.js
+++ index.js
@@ -51,50 +51,54 @@ class Enemy {
     damage() { this.hp--; } 
 }
 
-let enemy1 = new Enemy(5, './images/tv.png', './images/bg-tv.jpg', "150px", "120px", "155px", "270px");
+let enemy1 = new Enemy(5, './images/tv.png', './images/bg-tv.jpg', "150px", "120px", "140px", "280px");
 let enemy2 = new Enemy(10, './images/troop.png', './images/bg-troop.jpg', "150px", "300px", "100px", "300px");
 let enemy3 = new Enemy(15, './images/ship.png', './images/bg-ship.jpg', "450px", "300px", "100px", "170px");
-let enemy4 = new Enemy(20, './images/putin.png', './images/bg-putin.jpeg', "175px", "250px", "40px", "320px");
+let enemy4 = new Enemy(20, './images/putin.png', './images/bg-putin.jpeg', "175px", "250px", "15px", "320px");
 let enemy5 = new Enemy(1, './images/moskov.png', './images/bg-moskov.jpg', "450px", "350px", "0px", "150px");
 
 const enemies = [
     enemy1,
-    enemy2,
-    enemy3,
+/*     enemy2,
+    enemy3, */
     enemy4,
     enemy5
   ];
 
 function level(user, enemy) {
+    document.getElementById('levelNum').innerHTML = "Рівень:" + Number(user.level + 1);
     document.getElementById('user').innerHTML = "Ім'я:" + user.name;
     let score = document.getElementById('score');
-    score.innerHTML = "Рахунок:" + user.score;
-    document.getElementById('levelNum').innerHTML = "Рівень:" + Number(user.level + 1);
     let enemyHP = document.getElementById('enemyHP');
+    let enemyDiv = document.getElementById('enemy');
+    score.innerHTML = "Рахунок:" + user.score;
     enemyHP.innerHTML = "Здоров'я:" + enemy.getHP();
 
     document.getElementById('level-container').style.background = `url(${enemy.getBgImg()})`;
-    document.getElementById('level-container').style.backgroundSize = `cover`;
-    document.getElementById('enemy').style.background = `url(${enemy.getImg()})`;
-    document.getElementById('enemy').style.backgroundSize = `cover`;
-    document.getElementById('enemy').style.top = enemy.getTop();
-    document.getElementById('enemy').style.left = enemy.getLeft();
-    document.getElementById('enemy').style.height = enemy.getHeight();
-    document.getElementById('enemy').style.width = enemy.getWidth();
+    enemyDiv.style.cssText = `
+    background: url(${enemy.getImg()});
+    background-size: cover;
+    top: ${enemy.getTop()};
+    left: ${enemy.getLeft()};
+    height: ${enemy.getHeight()};
+    width: ${enemy.getWidth()};
+  `;
 
-
-    document.getElementById('enemy').addEventListener("click", function() {
-        enemy.damage();
+    function cliker() {
+                enemy.damage();
         user.score = user.score + 1;
         enemyHP.innerHTML = "Здоров'я:" + enemy.getHP();
         score.innerHTML = "Рахунок:" + user.score;
         if(enemy.getHP() == 0) {
             user.level++;
             alert("Вітаю, Ви виграли!");
-            
+            enemyDiv.removeEventListener("click", cliker)
             level(user, enemies[user.level])
         }
-    })
+    }
+
+
+    enemyDiv.addEventListener("click", cliker)
 }
 
 document.getElementById('startBtn').addEventListener("click", function(){
